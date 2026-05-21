8502e6875830df4dffb035565a2503170273a397
ngruenais <nyahprocontact@gmail.com>
2026-05-21 09:17:31 +0000

    Premier Commit SEANCE09

---
+++ apropos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A propos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h2>À Propos</h2>
+        <p><strong>Groupe : F </p>
+        <p><strong>Identifiant MMI :</strong> mmi25f06</p>
+        <p><strong>Prénom :</strong> Nyah</p>
+        <p><strong>Nom :</strong> Gruenais</p>
+    </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Site Herbergement - Gruenais Nyah</h1>
+     <nav>
+        <a href="index.php">Accueil</a>
+        <a href="apropos.php">À Propos</a>
+    </nav>
+</body>
+</html>
